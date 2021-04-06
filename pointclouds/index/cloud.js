3d958583658f1d2eca281aa951933ac294606d5b
unknown <pe_gasa@hotmail.com>
2021-04-06 15:36:51 +0200

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 63442969,
+    "boundingBox": {
+        "lx": 464859.38201904299,
+        "ly": 5163282.566986084,
+        "lz": 448.4590148925781,
+        "ux": 465010.57302856448,
+        "uy": 5163433.7579956059,
+        "uz": 599.6500244140625
+    },
+    "tightBoundingBox": {
+        "lx": 464859.3819,
+        "ly": 5163282.5669,
+        "lz": 448.4589,
+        "ux": 464985.726,
+        "uy": 5163433.7579,
+        "uz": 487.2537
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.3093525171279908,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
